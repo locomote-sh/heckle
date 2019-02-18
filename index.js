b34aafc468d462351209c3964f45bc767e3fdc8b
Julian Goacher <julian.goacher@innerfunction.com>
2019-02-18 17:43:35 +0000

    Changes to get sample project working.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-exports.build = require('./lib/build').build;
+const { build, loadSiteConfig } = require('./lib/build');
+exports.build = build;
+exports.loadSiteConfig = loadSiteConfig;
+
 exports.serve = require('./lib/serve').serve;
 exports.extensions = require('./lib/extensions');
